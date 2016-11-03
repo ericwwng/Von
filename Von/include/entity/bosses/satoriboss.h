e723faab8c32ecb0046312c9cc3b2bb28d8e7334
Eric <eric5531@yahoo.com>
2016-11-02 20:08:47 -0700

    Von
    The revival

---
+++ Von/include/entity/bosses/satoriboss.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "entity/boss.h"
+#include "entity/player.h"
+
+#include "utils/timer.h"
+
+#include "physics/AABB.h"
+
+#include "gfx/texture.h"
+#include "gfx/texutils.h"
+
+class Satori : public Boss
+{
+	public:
+		Satori();
+		~Satori();
+
+		void render();
+		void update(float deltaTime);
+
+		void phaseOne();
+		void randomPhase();
+
+		void random360();
+		void aimedShot();
+		void explodeAttack(Vector2f position);
+
+	private:
+		Timer m_songTimer;
+		Timer m_healthDecreaseTimer;
+		Timer m_speedUpTimer;
+		Timer m_collisionTimer;
+
+		unsigned int m_shootingRate;
+		unsigned int m_shootingSpeed;
+
+		Vector2f m_bulletSpawnPosition;
+		Projectile m_spawnCircle;
+};
---
+++ Von/include/game/level.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "game/gamestate.h"
+#include "game/map.h"
+#include "game/menu.h"
+#include "game/camera.h"
+
+#include "entity/player.h"
+#include "entity/bosses/satoriboss.h"
+#include "entity/bosses/pikachuboss.h"
+
+class Level : public GameState
+{
+    public:
+		Level(
+			std::string filename,
+			std::string worldName);
+        ~Level();
+
+        void render() const;
+        void update(
+			float deltaTime);
+        void handleEvents();
+
+    private:
+		Map* m_dungeon;
+		Boss* m_boss;
+};
